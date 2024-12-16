8e549ecb69b49660e6ffc97441701b77e0ac794f
Hajder Al-Remahy <hajduty@outlook.com>
2024-12-16 01:20:26 +0000

    initial commit

---
+++ Data/DbContext.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace backend.Data;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Book> Books { get; set; }
+}
---
+++ Service/TokenService.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace backend.Service;
+
+public class TokenService
+{
+
+}
